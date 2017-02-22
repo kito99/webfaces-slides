4fe322e0f636dcbdb436a15fcee874081a0cec5c
kito99 <kito.mann@virtua.com>
2017-02-22 16:48:09 -0500

    Updated to latest version of asciidoctor and reveal-js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,40 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-		exec: {
-		  readme: 'asciidoctor "README.asc"',
-		  session: 'asciidoctor -T asciidoctor-reveal.js/templates/slim "webfaces.asc"',
-            pdf: 'asciidoctor-pdf *.asc'		  
-		},
-        'connect': {
-            demo: {
-                options: {
-                    open: true,
-                    keepalive: true,
-                    port: 9091,
-					base: "."	
-                }
-            }
-        }
-    });
-
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-exec');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.registerTask('server', ['connect']);
-    
-	grunt.registerTask('build-readme', ['exec:readme']);	
-	grunt.registerTask('build-session', ['exec:session']);	
-	grunt.registerTask('build-pdf', ['exec:pdf']);		
-        
-	grunt.registerTask('default', ['build-readme', 'build-session', 'build-pdf']);	
-    
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+	grunt.initConfig({
+		exec: {
+			initBundle: 'bundle --path=.bundle/gems --binstubs=.bundle/.bin',
+			readme: 'bundle exec asciidoctor "README.asc"',
+			slides: 'bundle exec asciidoctor-revealjs "src/{*.asc,*.adoc}" -D out/html',
+			pdf: 'bundle exec asciidoctor asciidoctor-pdf src/{*.asc,*.adoc} -D out/pdf',
+			copyImages: 'cp -r src/images out/html',
+			copySlideLib: 'cp -r src/reveal.js out/html',
+			clean: 'rm -R out'
+		},
+		watch: {
+			files: ['src/**/*'],
+			tasks: ['default'],
+		},
+		'connect': {
+			demo: {
+				options: {
+					open: true,
+					keepalive: true,
+					port: 9091,
+					base: "./out/html"
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('init', ['exec:initBundle']);
+	grunt.registerTask('server', ['connect']);
+	grunt.registerTask('build-readme', ['exec:readme']);
+	grunt.registerTask('build-slides', ['clean','exec:slides','exec:copyImages','exec:copySlideLib']);
+	grunt.registerTask('build-pdf', ['exec:pdf']);
+	grunt.registerTask('clean', ['exec:clean']);
+	grunt.registerTask('default', ['build-readme', 'build-slides']);
 };
